0880c8ac5897464454bb5c9a937a2b584341b45a
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 19:49:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-string_text
+puts string_text
 end
 
 def all_holidays_with_bbq(holiday_hash)
